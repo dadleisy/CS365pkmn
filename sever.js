f0f5f908d6763acc9e4c4aa1e60314b601e3e938
Chad Leisenring <cleisenr@nmu.edu>
2018-11-02 12:34:52 -0400

    aa

--- sever.js
+++ sever.js
@@ -1,3 +1,21 @@
+// mongo.db server stuff
+var mongodb = require("mongodb");
+var MongoClient = mongodb.MongoClient;
+var ObjectID = mongodb.ObjectID;
+var client = new MongoClient("mongodb://localhost:27017", { useNewUrlParser: true });
+var db;
+
+// player one and two
+var p1 = [];
+var p2 = [];
+
+// database insert of Pokemon
+var pkmn = {name: name, hp: hp, atk: atk, def: def, spd: spd, m1: m1, m2: m2, m3: m3, m4: m4}
+var move = {moveName: name, power: power, acr: acr}
+db.pokemon.insert(new pkmn("Charizard", 153, 93, 98, 120, new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100)));
+db.pokemon.insert(new pkmn("Blastoise", 154, 92, 120, 98, new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100)));
+db.pokemon.insert(new pkmn("Venasaur", 155, 91, 103, 100, new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100)));
+
 var express = require("express");
 var app = express();
 var http = require("http");
