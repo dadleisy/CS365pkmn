e45b0b4e53e19e8357ee8c67552c921605b67d7d
Chad Leisenring <cleisenr@nmu.edu>
2018-11-02 12:31:35 -0400

    gg

--- chadServer.js
+++ chadServer.js
@@ -9,6 +9,6 @@ var p2 = [];
 
 var pkmn = {name: name, hp: hp, atk: atk, def: def, spd: spd, m1: m1, m2: m2, m3: m3, m4: m4}
 var move = {moveName: name, power: power, acr: acr}
-db.pokemon.insert(new pkmn("Charizard", 154, 84, 115, new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100)));
-db.pokemon.insert(new pkmn("Blastoise", 154, 84, 115, new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100)));
-db.pokemon.insert(new pkmn("Venasaur", 154, 84, 115, new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100)));
+db.pokemon.insert(new pkmn("Charizard", 153, 93, 98, 120, new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100)));
+db.pokemon.insert(new pkmn("Blastoise", 154, 92, 120, 98, new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100)));
+db.pokemon.insert(new pkmn("Venasaur", 155, 91, 103, 100, new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100), new move("tackle", 40, 100)));
