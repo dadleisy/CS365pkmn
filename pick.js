701a97881540b5c3c4753136c3205b7b77fa0cb9
Chad Leisenring <cleisenr@nmu.edu>
2018-11-07 13:05:21 -0500

    a

--- pick.js
+++ pick.js
@@ -1,13 +1,20 @@
 var socket = io();
 var p1Pick = false;
+var p2Pick = false;
 
 function setup() {
 
     $("pickPKMN").click( function() {
-        socket.emit("setPKMN", $("#train").val(), $("pkmn1").val(), $("pkmn2").val(), $("pkmn3").val());
-        $("#pickPage").hide();
-        $("#playerOneMenu").show();
-        $("#playerTwoMenu").show();
+        if (!p1Pick) {
+            socket.emit("setPKMN", $("#train").val(), $("pkmn1").val(), $("pkmn2").val(), $("pkmn3").val());
+            //$("#pickPage").hide();
+            //$("#playerOneMenu").show();
+            //$("#playerTwoMenu").show();
+        }
+        else {
+            
+        }
+
     });
 
 
